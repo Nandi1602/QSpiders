defe728fd635611766ad8d1dd5142cc4a7586168
Nandeesha <nandeesha.s@gmail.com>
2016-07-10 14:25:11 +0530

    uploading testNG project.

---
+++ src/com/qspiders/github/GitHubDemo.java
@@ -0,0 +1,14 @@
+package com.qspiders.github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHubDemo {
+	
+	@Test
+	public void testA()
+	{
+		Reporter.log("running testA method");
+	}
+	
+}
